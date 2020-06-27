0a79f911ceba5364942b67a82e8d08a6ab54ab28
Jiraiya665 <jiraiya665@gmail.com>
2020-06-27 17:53:03 +1000

    added onStart and onFinish method for listenres

--- org/scorelab/TAF/src/listeners/listeners.java
+++ org/scorelab/TAF/src/listeners/listeners.java
@@ -1,4 +1,22 @@
 package listeners;
 
-public class listeners {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.*;
+
+public class listeners implements ITestListener, ISuiteListener{
+    private String strLoggerMsg;
+    private String strClassName;
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestNGListener.class);
+    
+     @Override
+    public void onStart(ISuite arg0) {
+        LOGGER.info("About To Begin Executing Suite : " + arg0.getName());
+    }
+
+    @Override
+    public void onFinish(ISuite arg0) {
+        LOGGER.info("About To End Executing Suite : " + arg0.getName());
+
+    }
 }
