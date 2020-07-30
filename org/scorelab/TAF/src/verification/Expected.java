fce503ab18afb4b44d68a02c261b36e5381d13d5
Jiraiya665 <jiraiya665@gmail.com>
2020-07-30 15:37:12 +1000

    created expected.java

---
+++ org/scorelab/TAF/src/verification/Expected.java
@@ -0,0 +1,14 @@
+package com.framework.qa.uicore.verification;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Expected {
+    
+
+}
+
+
