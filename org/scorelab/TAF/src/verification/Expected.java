17680b5784d96b79551886cc857232ee2a333e50
Jiraiya665 <jiraiya665@gmail.com>
2020-07-30 15:38:13 +1000

    added require methds

--- org/scorelab/TAF/src/verification/Expected.java
+++ org/scorelab/TAF/src/verification/Expected.java
@@ -7,7 +7,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Expected {
-    
+    private static final Logger LOGGER = LoggerFactory.getLogger(Expected.class);
+
+    private HashMap<String, Object> results = null;
+
+    public Expected() {
+        results = new HashMap<String, Object>();
+    }
+
+    public void setExpectedResult(String key, Object value) {
+        results.put(key, value);
+    }
+
+    public void clearAndSetExpectedResult(String key, Object value) {
+        results.clear();
+        results.put(key, value);
+    }
+
+    public Map<String, Object> getResults() {
+        return results;
+    }
 
 }
 
